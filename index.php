06551d0ffb054acb16b7aa6cb8665d555cf02103
Luuk de Vlieger <luuk@trendwerk.nl>
2015-04-27 23:42:04 +0200

    Added more animations

--- index.php
+++ index.php
@@ -12,11 +12,21 @@
 	<body>
 		<div id="container">
 			<h1>Animated menu icons &nbsp;&mdash;&nbsp; take your pick.</h1>
-			<div class="icon" id="icon1">
-				<div class="line" id="line1"></div>
-				<div class="line" id="line2"></div>
-				<div class="line" id="line3"></div>
-			</div>
+
+			<?php
+			for( $i = 1; $i <= 10; $i++ ) {
+			?>
+
+				<div class="icon" id="icon<?php echo $i; ?>">
+					<div class="line" id="line1"></div>
+					<div class="line" id="line2"></div>
+					<div class="line" id="line3"></div>
+				</div>
+
+			<?php
+			}
+			?>
+
 		</div>
 		<script src="http://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
 		<script src="script.js" type="text/javascript"></script>
