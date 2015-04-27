bfe08237658b77c18d7990e2f1df6ebf6346fba3
Luuk de Vlieger <luuk@trendwerk.nl>
2015-04-27 18:11:05 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		<link href="http://fonts.googleapis.com/css?family=Palanquin:300" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="style.css" rel="stylesheet" type="text/css" />
+		<title>Animated menu icons</title>
+	</head>
+
+	<body>
+		<div id="container">
+			<h1>Animated menu icons &nbsp;&mdash;&nbsp; take your pick.</h1>
+			<div class="icon" id="icon1">
+				<div class="line" id="line1"></div>
+				<div class="line" id="line2"></div>
+				<div class="line" id="line3"></div>
+			</div>
+		</div>
+		<script src="http://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
+		<script src="script.js" type="text/javascript"></script>
+	</body>
+
+</html>
