0399e8266d3d80491614dfeca78d45b09f3b0d7c
Luuk de Vlieger <luuk@trendwerk.nl>
2016-02-12 01:44:33 +0100

    Repo name update

--- partials/tabs.php
+++ partials/tabs.php
@@ -4,14 +4,14 @@
 	<div class="tab-title" data-tab=".tab-html">HTML</div>
 	<div class="tab-content tab-js active">
 		<p>Grab the script (jQuery) from GitHub:</p>
-		<a target="_blank" href="https://github.com/luukdv/animated-menu-icons/blob/master/js/icon.js">jQuery</a>
+		<a target="_blank" href="https://github.com/luukdv/menu-icons/blob/master/js/icon.js">jQuery</a>
 	</div>
 	<div class="tab-content tab-css">
 		<p>Pick up the styling (SCSS) from GitHub:</p>
-		<a target="_blank" href="https://github.com/luukdv/animated-menu-icons/blob/master/css/icons/icon<?php echo $i; ?>.scss">SCSS</a>
+		<a target="_blank" href="https://github.com/luukdv/menu-icons/blob/master/css/icons/icon<?php echo $i; ?>.scss">SCSS</a>
 	</div>
 	<div class="tab-content tab-html">
 		<p>Get the markup (HTML) from GitHub:</p>
-		<a target="_blank" href="https://github.com/luukdv/animated-menu-icons/blob/master/partials/icons/<?php echo $type; ?>/icon<?php echo $i; ?>.html">HTML</a>
+		<a target="_blank" href="https://github.com/luukdv/menu-icons/blob/master/partials/icons/<?php echo $type; ?>/icon<?php echo $i; ?>.html">HTML</a>
 	</div>
 </div>
