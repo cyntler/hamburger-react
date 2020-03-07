d5c0fb478fd7b42f5d98f02588adaebbab4ac28f
Luuk de Vlieger <info@luuk.site>
2020-03-07 13:25:17 +0100

    :recycle: Simplify hamburger component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 
-const area = 48
-const width = 34
-const room = Math.round((area - width) / 2)
-const line = Math.round(width / 10)
-const margin = Math.round(width / 4)
-const height = line * 3 + margin * 2
-const topOffset = Math.round((area - height) / 2)
-const move = width / 4.04
-const transition = 'transform 0.4s cubic-bezier(0, 0, 0, 1)'
-
 export const Hamburger = ({ onToggle }) => {
+  const area = 48
+  const width = 34
+  const room = Math.round((area - width) / 2)
+  const line = Math.round(width / 10)
+  const margin = Math.round(width / 4)
+  const height = line * 3 + margin * 2
+  const topOffset = Math.round((area - height) / 2)
+  const move = width / 4.04
+  const transition = 'transform 0.4s cubic-bezier(0, 0, 0, 1)'
+
   const [toggled, toggle] = useState(false)
 
   const handler = () => {
@@ -21,17 +21,26 @@ export const Hamburger = ({ onToggle }) => {
     toggle(!toggled)
   }
 
+  const lineStyles = {
+    background: 'white',
+    height: `${line}px`,
+    left: `${room}px`,
+    position: 'absolute',
+    transition: 'transform 0.4s cubic-bezier(0, 0, 0, 1)',
+    width: `${width}px`,
+  }
+
   return (
     <div
+      className="hamburger-react"
       style={{
-        userSelect: 'none',
         cursor: 'pointer',
         height: `${area}px`,
-        left: `-${room}px`,
-        top: `-${room}px`,
+        outline: 'none',
         position: 'relative',
         transform: `${toggled ? 'rotate(-90deg)' : 'none'}`,
         transition: `${transition}`,
+        userSelect: 'none',
         width: `${area}px`,
       }}
       onClick={handler}
@@ -39,32 +48,21 @@ export const Hamburger = ({ onToggle }) => {
       tabIndex="0"
       onKeyUp={e => (e.key === 13 || e.keyCode === 13) && handler()}
     >
-      <Line styles={{
+      <div style={{
+        ...lineStyles,
         top: `${topOffset}px`,
         transform: `${toggled ? `rotate(-45deg) translate(-${move}px, ${move}px)` : 'none'}`,
-      }}
-      />
-      <Line styles={{
+      }} />
+      <div style={{
+        ...lineStyles,
         top: `${topOffset + line + margin}px`,
         transform: `${toggled ? 'scale(0, 1)' : 'none'}`,
-      }}
-      />
-      <Line styles={{
+      }} />
+      <div style={{
+        ...lineStyles,
         top: `${topOffset + line * 2 + margin * 2}px`,
         transform: `${toggled ? `rotate(45deg) translate(-${move}px, -${move}px)` : 'none'}`,
-      }}
-      />
+      }} />
     </div>
   )
 }
-
-const Line = ({ styles }) => (
-  <div style={Object.assign({
-    background: 'white',
-    height: `${line}px`,
-    left: `${room}px`,
-    position: 'absolute',
-    transition: 'transform 0.4s cubic-bezier(0, 0, 0, 1)',
-    width: `${width}px`,
-  }, styles)} />
-)
