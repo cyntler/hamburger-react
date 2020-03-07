7a33ea1d557227c9a055d3d25a504a2420cb2aad
Luuk de Vlieger <info@luuk.site>
2020-03-07 12:42:53 +0100

    :construction: Main JS file

---
+++ example/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Hamburger } from '../src'
+
+const App = () => <Hamburger />
+
+ReactDOM.render(<App />, document.querySelector('.main'))
