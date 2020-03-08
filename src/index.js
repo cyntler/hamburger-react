4c31531b856b010e6cd00828cc177f3b6d686ea3
Luuk de Vlieger <info@luuk.site>
2020-03-08 16:02:05 +0100

    :recycle: Refactor variables

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ import React, { useState } from 'react'
 
 export const Hamburger = ({
   onToggle,
-  size = 'm',
+  size = 36,
 }) => {
   const area = 48
-  const sizes = { xs: 16, s: 24, m: 32, l: 40, xl: 48 }
-  const width = sizes[size] ? sizes[size] : sizes['m']
+  const width = Math.max(12, Math.min(area, size))
   const room = Math.round((area - width) / 2)
-  const barHeight = Math.round(width / 12)
-  const margin = Math.round(width / 4)
+  const barHeightRaw = width / 12
+  const barHeight = Math.round(barHeightRaw)
+  const marginRaw = width / 4.5
+  const margin = Math.round(marginRaw)
   const height = barHeight * 3 + margin * 2
   const topOffset = Math.round((area - height) / 2)
-  const translations = { xs: 4.55, s: 4.25, m: 4.1, l: 4.35, xl: 4.25 }
-  const move = width / (translations[size] ? translations[size] : translations['m'])
+  const deviation = (barHeightRaw - barHeight) + (marginRaw - margin)
+  const move = (width / 4.6325) - (deviation / (4 / 3))
   const transition = 'transform 0.4s cubic-bezier(0, 0, 0, 1)'
 
-  const [toggled, toggle] = useState(true)
+  const [toggled, toggle] = useState(false)
 
   const handler = () => {
     if (onToggle) {
