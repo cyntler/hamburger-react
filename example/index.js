4e5531e3db9e11ad21607648b500a6c0bddd5160
Luuk de Vlieger <info@luuk.site>
2020-03-08 20:02:08 +0100

    :lipstick: Example card styles

--- example/index.js
+++ example/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom'
 import { Hamburger } from '../src'
 
 const App = () => (
-  <div className="rounded-lg shadow-xl p-8 px-12 bg-white">
-    <h2 className="leading-snug text-center mb-6 text-xl font-medium">Default</h2>
-    <div className="bg-gray-800 rounded shadow-lg p-8 px-12 -mb-16">
+  <div className="bg-gray-800 rounded-lg shadow-xl py-8 px-12 flex items-center">
+    <div className="flex flex-col items-center border-r-2 border-gray-700 pr-12 mr-12">
+      <h2 className="leading-snug text-center text-xl font-semibold">Default</h2>
+      <h3 className="leading-snug text-center text-gray-600 mb-4 text-sm">Direction: left</h3>
       <Hamburger color="white" />
     </div>
+    <code className="text-sm px-6 py-4 rounded font-bold bg-indigo-800 shadow-inner">
+      {'<Hamburger />'}
+    </code>
   </div>
 )
 
