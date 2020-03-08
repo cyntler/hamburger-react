dd5d239556b96e96f954932c5e6242dab32b6c0f
Luuk de Vlieger <info@luuk.site>
2020-03-08 18:36:36 +0100

    :construction: Example page

--- example/index.js
+++ example/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Hamburger } from '../src'
 
-const App = () => <Hamburger color="white" />
+const App = () => (
+  <div className="rounded-lg shadow-xl p-8 px-12 bg-white">
+    <h2 className="leading-snug text-center mb-6 text-xl font-medium">Default</h2>
+    <div className="bg-gray-800 rounded shadow-lg p-8 px-12 -mb-16">
+      <Hamburger color="white" />
+    </div>
+  </div>
+)
 
 ReactDOM.render(<App />, document.querySelector('.main'))
