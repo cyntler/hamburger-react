141c7ce24b37b7d28666c8c92e9c1a39e76a9214
Luuk de Vlieger <info@luuk.site>
2020-03-09 19:30:24 +0100

    :lipstick: Example page

--- example/index.js
+++ example/index.js
@@ -16,18 +16,63 @@ const Main = () => (
         <div>{'/>'}</div>
       </>
     </Card>
+    <Card title="Default" subtitle="Direction: left">
+      <Hamburger color="white" />
+      {'<Hamburger />'}
+    </Card>
+    <Card title="Default" subtitle="Direction: right">
+      <Hamburger color="white" direction="right" />
+      <>
+        <div>{'<Hamburger'}</div>
+        <div className="ml-4">direction="right"</div>
+        <div>{'/>'}</div>
+      </>
+    </Card>
   </>
 )
 
 const Card = ({ children, title, subtitle, code }) => (
-  <div className="bg-gray-800 rounded-lg shadow-xl py-8 px-12 flex items-center">
-    <div className="flex flex-col items-center border-r-2 border-gray-700 pr-12 mr-12">
+  <div className="
+    bg-gray-800
+    rounded-lg
+    shadow-xl
+    py-8
+    px-8
+    md:px-12
+    flex
+    flex-col
+    xl:flex-row
+    items-center
+  ">
+    <div className="
+      flex
+      flex-col
+      items-center
+      xl:border-r-2
+      xl:border-gray-700
+      xl:pr-12
+      xl:mr-12
+      mb-6
+      xl:mb-0
+    ">
       <h2 className="leading-snug text-center text-xl font-semibold">{title}</h2>
       <h3 className="leading-snug text-center text-gray-600 mb-4 text-sm">{subtitle}</h3>
       {children[0]}
     </div>
-    <code className="text-sm px-6 py-4 rounded bg-indigo-800 shadow-inner">
-    {children[1]}
+    <code className="
+      w-full
+      xl:w-64
+      max-w-xs
+      flex
+      justify-center
+      text-sm
+      px-6
+      py-4
+      rounded
+      bg-indigo-800
+      shadow-inner
+    ">
+      <div>{children[1]}</div>
     </code>
   </div>
 )
