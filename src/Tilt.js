5ff8bce31ef9b51592308bce6bae1f229f32f4b1
Luuk de Vlieger <info@luuk.site>
2020-03-10 19:34:23 +0100

    :recycle: Refactor to types

---
+++ src/Tilt.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export const Tilt = ({ o }) => (
+  <div
+    className="hamburger-react"
+    style={{
+      ...o.burgerStyles,
+      transform: `${o.toggled
+        ? `rotate(${90 * (o.isLeft ? -1 : 1)}deg)`
+        : 'none'
+      }`,
+    }}
+    onClick={o.handler}
+    role="button"
+    tabIndex="0"
+    onKeyUp={e => (e.key === 13 || e.keyCode === 13) && o.handler()}
+  >
+    <div style={{
+      ...o.barStyles,
+      top: `${o.topOffset}px`,
+      transform: `${o.toggled
+        ? `rotate(${45 * (o.isLeft ? -1 : 1)}deg) translate(${o.move * (o.isLeft ? -1 : 1)}px, ${o.move}px)`
+        : 'none'
+      }`,
+    }} />
+    <div style={{
+      ...o.barStyles,
+      top: `${o.topOffset + o.barHeight + o.margin}px`,
+      transform: `${o.toggled ?
+        'scale(0, 1)'
+        : 'none'
+      }`,
+    }} />
+    <div style={{
+      ...o.barStyles,
+      top: `${o.topOffset + o.barHeight * 2 + o.margin * 2}px`,
+      transform: `${o.toggled
+        ? `rotate(${45 * (o.isLeft ? 1 : -1)}deg) translate(${o.move * (o.isLeft ? -1 : 1)}px, ${o.move * -1}px)`
+        : 'none'
+      }`,
+    }} />
+  </div>
+)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
+import { Tilt } from './Tilt'
+
+const area = 48
+const transition = 'transform 0.4s cubic-bezier(0, 0, 0, 1)'
+const translate = 4.6325
 
 export const Hamburger = ({
   color = '#000',
   direction = 'left',
   rounded = false,
   size = 32,
+  type="tilt",
   onToggle,
 }) => {
   const [toggled, toggle] = useState(false)
 
-  const area = 48
   const width = Math.max(12, Math.min(area, size))
   const room = Math.round((area - width) / 2)
 
@@ -22,10 +27,27 @@ export const Hamburger = ({
   const topOffset = Math.round((area - height) / 2)
 
   const deviation = (barHeightRaw - barHeight) + (marginRaw - margin)
-  const translate = 4.6325
   const move = (width / translate) - (deviation / (4 / 3))
-  const left = direction === 'left'
-  const transition = 'transform 0.4s cubic-bezier(0, 0, 0, 1)'
+  const isLeft = direction === 'left'
+
+  const barStyles = {
+    background: color,
+    height: `${barHeight}px`,
+    left: `${room}px`,
+    position: 'absolute',
+    transition,
+    width: `${width}px`,
+  }
+
+  const burgerStyles = {
+    cursor: 'pointer',
+    height: `${area}px`,
+    outline: 'none',
+    position: 'relative',
+    transition,
+    userSelect: 'none',
+    width: `${area}px`,
+  }
 
   const handler = () => {
     if (onToggle) {
@@ -35,64 +57,22 @@ export const Hamburger = ({
     toggle(!toggled)
   }
 
-  const barStyles = {
-    background: color,
-    height: `${barHeight}px`,
-    left: `${room}px`,
-    position: 'absolute',
-    transition: 'transform 0.4s cubic-bezier(0, 0, 0, 1)',
-    width: `${width}px`,
-  }
-
   if (rounded) {
     barStyles['borderRadius'] = '9em'
   }
 
-  return (
-    <div
-      className="hamburger-react"
-      style={{
-        cursor: 'pointer',
-        height: `${area}px`,
-        outline: 'none',
-        position: 'relative',
-        transform: `${toggled
-          ? `rotate(${90 * (left ? -1 : 1)}deg)`
-          : 'none'
-        }`,
-        transition: `${transition}`,
-        userSelect: 'none',
-        width: `${area}px`,
-      }}
-      onClick={handler}
-      role="button"
-      tabIndex="0"
-      onKeyUp={e => (e.key === 13 || e.keyCode === 13) && handler()}
-    >
-      <div style={{
-        ...barStyles,
-        top: `${topOffset}px`,
-        transform: `${toggled
-          ? `rotate(${45 * (left ? -1 : 1)}deg) translate(${move * (left ? -1 : 1)}px, ${move}px)`
-          : 'none'
-        }`,
+  switch (type) {
+    default:
+      return <Tilt o={{
+        barHeight,
+        barStyles,
+        burgerStyles,
+        handler,
+        isLeft,
+        margin,
+        move,
+        toggled,
+        topOffset,
       }} />
-      <div style={{
-        ...barStyles,
-        top: `${topOffset + barHeight + margin}px`,
-        transform: `${toggled ?
-          'scale(0, 1)'
-          : 'none'
-        }`,
-      }} />
-      <div style={{
-        ...barStyles,
-        top: `${topOffset + barHeight * 2 + margin * 2}px`,
-        transform: `${toggled
-          ? `rotate(${45 * (left ? 1 : -1)}deg) translate(${move * (left ? -1 : 1)}px, ${move * -1}px)`
-          : 'none'
-        }`,
-      }} />
-    </div>
-  )
+  }
 }
