5a3a32b38868862a81f179e4d0d5b78d79cd4612
Luuk de Vlieger <info@luuk.site>
2020-03-11 10:16:37 +0100

    :lipstick: New layout

--- example/index.js
+++ example/index.js
@@ -5,39 +5,34 @@ import { Fade } from '../src'
 
 const Main = () => (
   <>
-    <Card title="Tilt" subtitle="Direction: left">
-      <Hamburger size={36} color="white" />
-      <div>&lt;Hamburger /&gt;</div>
-    </Card>
-    <Card title="Tilt" subtitle="Direction: right">
+    <Card title="Tilt">
       <Hamburger size={36} color="white" direction="right" />
-      <>
-        <div>&lt;Hamburger</div>
-        <div>&nbsp;&nbsp;direction="right"</div>
-        <div>/&gt;</div>
-      </>
+      <div>
+        Import
+        <span className="text-white"> Hamburger </span>
+        from '
+        <span className="text-green-400">hamburger-react</span>
+        '
+      </div>
+      <Hamburger size={36} color="white" />
     </Card>
-    <Card title="Fade" subtitle="Direction: left">
-      <Fade size={36} color="white" />
-      <>
-        <div>&lt;Hamburger</div>
-        <div>&nbsp;&nbsp;type="fade"</div>
-        <div>/&gt;</div>
-      </>
-    </Card>
-    <Card title="Fade" subtitle="Direction: right">
+    <Card title="Fade">
       <Fade size={36} color="white" direction="right" />
-      <>
-        <div>&lt;Hamburger</div>
-        <div>&nbsp;&nbsp;type="fade"</div>
-        <div>&nbsp;&nbsp;direction="right"</div>
-        <div>/&gt;</div>
-      </>
+      <div>
+        Import {'{'}
+        <span className="text-white"> Fade </span>
+        as
+        <span className="text-white"> Hamburger </span>
+        {'}'} from '
+        <span className="text-green-400">hamburger-react</span>
+        '
+      </div>
+      <Fade size={36} color="white" />
     </Card>
   </>
 )
 
-const Card = ({ children, title, subtitle, code }) => (
+const Card = ({ children, title, code }) => (
   <div className="
     bg-gray-800
     rounded-lg
@@ -46,41 +41,62 @@ const Card = ({ children, title, subtitle, code }) => (
     px-8
     md:px-12
     flex
+    mb-6
     flex-col
-    xl:flex-row
     items-center
   ">
-    <div className="
-      flex
-      flex-col
-      items-center
-      xl:border-r-2
-      xl:border-gray-700
-      xl:pr-12
-      xl:mr-12
-      mb-6
-      xl:mb-0
-    ">
-      <h2 className="leading-snug text-center text-xl font-semibold">{title}</h2>
-      <h3 className="leading-snug text-center text-gray-600 mb-4 text-sm">{subtitle}</h3>
-      {children[0]}
+    <h2 className="mb-2 leading-snug text-center font-semibold flex items-center">
+      <span className="text-l mr-4 text-gray-400">Hamburger type:</span>
+      <span className="text-xl">{title}</span>
+    </h2>
+    <div className="flex w-full items-center">
+      <div className="
+        flex
+        flex-col
+        items-center
+        lg:border-r-2
+        lg:border-gray-700
+        lg:pr-12
+        lg:mr-12
+        flex-shrink-0
+        lg:mb-0
+      ">
+        <h3 className="leading-snug text-center text-gray-500 font-semibold mb-2 text-sm">
+          Direction: right
+        </h3>
+        {children[0]}
+      </div>
+      <code className="
+        text-blue-300
+        flex
+        flex-grow
+        justify-center
+        text-xs
+        px-6
+        py-4
+        rounded
+        bg-indigo-800
+        shadow-inner
+      ">
+        {children[1]}
+      </code>
+      <div className="
+        flex
+        flex-col
+        items-center
+        lg:border-l-2
+        lg:border-gray-700
+        lg:pl-12
+        lg:ml-12
+        flex-shrink-0
+        lg:mb-0
+      ">
+        <h3 className="leading-snug text-center text-gray-500 font-semibold mb-2 text-sm">
+          Direction: left
+        </h3>
+        {children[2]}
+      </div>
     </div>
-    <code className="
-      text-indigo-100
-      w-full
-      xl:w-64
-      max-w-xs
-      flex
-      justify-center
-      text-sm
-      px-6
-      py-4
-      rounded
-      bg-indigo-800
-      shadow-inner
-    ">
-      <div>{children[1]}</div>
-    </code>
   </div>
 )
 
