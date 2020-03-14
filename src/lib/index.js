6e2c47555b5dcf5fca21ad937b444dd44dd47d63
Luuk de Vlieger <info@luuk.site>
2020-03-14 11:47:01 +0100

    :bug: Re-add missing index after migration

---
+++ src/lib/index.js
@@ -0,0 +1,12 @@
+import { Tilt as Hamburger } from './Tilt'
+
+import { Fade } from './Fade'
+import { Sling } from './Sling'
+import { Spiral } from './Spiral'
+import { Squash } from './Squash'
+import { Turn } from './Turn'
+import { Twirl } from './Twirl'
+
+export default Hamburger
+
+export { Fade, Sling, Spiral, Squash, Turn, Twirl }
