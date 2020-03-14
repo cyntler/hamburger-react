fb92e44253b949a58116bfd07273863d985378dc
Luuk de Vlieger <info@luuk.site>
2020-03-14 12:41:45 +0100

    :art: Simpler wrap

--- src/src/components/wrap.js
+++ src/src/components/wrap.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default ({ children }) => (
-  <div className="mx-auto px-5 md:px-10 max-w-screen-lg">
-    <div className="flex items-center flex-col">{children}</div>
+  <div className="mx-auto px-5 md:px-10 max-w-screen-lg flex items-center flex-col">
+    {children}
   </div>
 )
