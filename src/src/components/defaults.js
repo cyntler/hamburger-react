8bade15de776023ac8e07481cf135c21d763dddc
Luuk de Vlieger <info@luuk.site>
2020-03-14 12:11:12 +0100

    :art: Format

--- src/src/components/defaults.js
+++ src/src/components/defaults.js
@@ -34,7 +34,8 @@ export default () => (
         &nbsp;&nbsp;
         <span className="text-purple-400">color</span>
         ="
-        <span className="text-green-400">#000</span>"
+        <span className="text-green-400">{'#'}000</span>
+        "
         <span className="hidden md:inline text-sm text-indigo-400">
           &nbsp;// line color
         </span>
@@ -44,7 +45,8 @@ export default () => (
         &nbsp;&nbsp;
         <span className="text-purple-400">direction</span>
         ="
-        <span className="text-green-400">left</span>"
+        <span className="text-green-400">left</span>
+        "
         <span className="hidden md:inline text-sm text-indigo-400">
           &nbsp;// animation direction
         </span>
@@ -52,7 +54,8 @@ export default () => (
 
       <div className="flex items-center">
         &nbsp;&nbsp;
-        <span className="text-purple-400">rounded</span>={'{'}
+        <span className="text-purple-400">rounded</span>
+        ={'{'}
         <span className="text-green-400">false</span>
         {'}'}
         <span className="hidden md:inline text-sm text-indigo-400">
@@ -62,7 +65,8 @@ export default () => (
 
       <div className="flex items-center">
         &nbsp;&nbsp;
-        <span className="text-purple-400">size</span>={'{'}
+        <span className="text-purple-400">size</span>
+        ={'{'}
         <span className="text-green-400">32</span>
         {'}'}
         <span className="hidden md:inline text-sm text-indigo-400">
@@ -72,13 +76,15 @@ export default () => (
 
       <div className="flex items-center">
         &nbsp;&nbsp;
-        <span className="text-purple-400">onToggle</span>={'{'}
+        <span className="text-purple-400">onToggle</span>
+        ={'{'}
         <span className="text-green-400">null</span>
         {'}'}
         <span className="hidden md:inline text-sm text-indigo-400">
           &nbsp;// callback
         </span>
       </div>
+
       <div>{'/>'}</div>
     </code>
   </>
