f563bb84ae5ad9c0d8e923fbfb64b91e3a8fb66d
Luuk de Vlieger <info@luuk.site>
2020-03-14 16:46:45 +0100

    :sparkles: Add other examples

--- src/src/components/properties.js
+++ src/src/components/properties.js
@@ -20,60 +20,83 @@ export default () => (
     <div className="w-full max-w-xl">
       <Example>
         <Property name="color" type="string" />
-        <Hamburger color="white" size={34} />
+        <Hamburger color="#4FD1C5" size={34} />
+        <div>
+          {'<'}
+          <span className="text-yellow-400">Hamburger</span>
+          &nbsp;
+          <span className="text-purple-400">color</span>="
+          <span className="text-green-400">#4FD1C5</span>
+          {'" />'}
+        </div>
       </Example>
 
       <Example>
         <Property name="direction" type="string" />
-        <Hamburger color="white" size={26} />
+        <Hamburger color="white" direction="right" size={26} />
+        <div>
+          {'<'}
+          <span className="text-yellow-400">Hamburger</span>
+          &nbsp;
+          <span className="text-purple-400">direction</span>="
+          <span className="text-green-400">right</span>
+          {'" />'}
+        </div>
       </Example>
 
       <Example>
         <Property name="rounded" type="boolean" />
         <Hamburger color="white" rounded size={42} />
+        <div>
+          {'<'}
+          <span className="text-yellow-400">Hamburger</span>
+          &nbsp;
+          <span className="text-purple-400">rounded</span>
+          {' />'}
+        </div>
       </Example>
 
       <Example>
         <Property name="size" type="integer" />
         <Hamburger color="white" size={20} />
-        <>
-          <div>
-            {'<'}
-            <span className="text-yellow-400">Hamburger</span>
-            &nbsp;
-            <span className="text-purple-400">size</span>
-            ={'{'}
-            <span className="text-green-400">20</span>
-            {'} />'}
-          </div>
-        </>
+        <div>
+          {'<'}
+          <span className="text-yellow-400">Hamburger</span>
+          &nbsp;
+          <span className="text-purple-400">size</span>={'{'}
+          <span className="text-green-400">20</span>
+          {'} />'}
+        </div>
       </Example>
 
       <Example last>
         <Property name="onToggle" type="function" />
         <Hamburger color="white" size={30} />
         <>
-          <div>{'<'}<span className="text-yellow-400">Hamburger</span></div>
+          <div>
+            {'<'}
+            <span className="text-yellow-400">Hamburger</span>
+          </div>
 
           <div>
             &nbsp;&nbsp;
-            <span className="text-purple-400">onToggle</span>
-            ={'{'}
+            <span className="text-purple-400">onToggle</span>={'{'}
             <span className="text-orange-400">toggled</span>
             &nbsp;=>{' {'}
           </div>
 
           <div>
             &nbsp;&nbsp;&nbsp;&nbsp;if (
-            <span className="text-orange-400">toggled</span>
-            ) {'{'}
+            <span className="text-orange-400">toggled</span>) {'{'}
           </div>
 
           <div className="text-indigo-400">
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;// open a menu
           </div>
 
-          <div>&nbsp;&nbsp;&nbsp;&nbsp;{'}'} else {'{'}</div>
+          <div>
+            &nbsp;&nbsp;&nbsp;&nbsp;{'}'} else {'{'}
+          </div>
 
           <div className="text-indigo-400">
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;// close a menu
