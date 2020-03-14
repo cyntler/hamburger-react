acc03c4bf010b7a64aea74a8345308bc7906bf99
Luuk de Vlieger <info@luuk.site>
2020-03-14 20:27:58 +0100

    :sparkles: Add footer

---
+++ src/src/components/footer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default () => (
+  <div className="w-full flex justify-between text-sm text-gray-600">
+    <p>
+      hamburger-react is a project by{' '}
+      <a
+        className="underline font-medium"
+        href="https://www.luuk.site"
+        target="_blank" // eslint-disable-line react/jsx-no-target-blank
+        rel="noopener"
+      >
+        Luuk
+      </a>
+      .
+    </p>
+    <div>
+      <a
+        className="underline font-medium px-4"
+        href="https://www.npmjs.com/package/react-hamburger"
+        target="_blank" // eslint-disable-line react/jsx-no-target-blank
+        rel="noopener"
+      >
+        NPM
+      </a>
+      <a
+        className="underline font-medium px-4"
+        href="https://github.com/luukdv/hamburger-react"
+        target="_blank" // eslint-disable-line react/jsx-no-target-blank
+        rel="noopener"
+      >
+        GitHub
+      </a>
+      <a
+        className="underline font-medium px-4"
+        href="https://www.luuk.site"
+        target="_blank" // eslint-disable-line react/jsx-no-target-blank
+        rel="noopener"
+      >
+        Contact
+      </a>
+    </div>
+  </div>
+)
--- src/src/pages/index.js
+++ src/src/pages/index.js
@@ -1,6 +1,7 @@
 import Cards from '../components/cards'
 import Properties from '../components/properties'
 import Installation from '../components/installation'
+import Footer from '../components/footer'
 import Information from '../components/information'
 import Title from '../components/title'
 import Wrap from '../components/wrap'
@@ -27,9 +28,15 @@ export default () => (
       </Wrap>
     </div>
 
-    <div className="mb-16 md:mb-32">
+    <div className="mb-16 md:mb-24">
       <Wrap>
         <Information />
+      </Wrap>
+    </div>
+
+    <div className="bg-gray-800 md:pt-12 md:pb-16">
+      <Wrap>
+        <Footer />
       </Wrap>
     </div>
   </>
