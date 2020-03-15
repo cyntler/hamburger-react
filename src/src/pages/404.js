1d850d949def867db7429eb0306fc9878ed02992
Luuk de Vlieger <info@luuk.site>
2020-03-15 13:19:18 +0100

    :sparkles: 404 page

--- src/src/pages/404.js
+++ src/src/pages/404.js
@@ -1,3 +1,25 @@
 import React from 'react'
+import Wrap from '../components/wrap'
 
-export default () => <h1>404</h1>
+export default () => (
+  <Wrap>
+    <div className="flex h-screen items-center">
+      <h1
+        className="
+          leading-snug
+          text-5xl
+          mb-8
+          md:mb-16
+          lg:mb-32
+          font-medium
+          text-center
+        "
+      >
+        404
+        <span className="ml-4" role="img" aria-label="Shrug">
+          🤷‍♂️
+        </span>
+      </h1>
+    </div>
+  </Wrap>
+)
