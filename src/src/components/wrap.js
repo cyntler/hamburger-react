597f3f02908551c95e89276577a9379912a967d5
Luuk de Vlieger <info@luuk.site>
2020-03-18 14:09:43 +0100

    :wrench: Wide wrap option

--- src/src/components/wrap.js
+++ src/src/components/wrap.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default ({ children }) => (
-  <div className="mx-auto px-5 sm:px-10 max-w-screen-lg flex items-center flex-col">
+export default ({ children, wide }) => (
+  <div
+    className={`
+    mx-auto
+    px-5
+    sm:px-10
+    max-w-screen-${wide ? 'xl' : 'lg'}
+    flex
+    items-center
+    flex-col
+  `}
+  >
     {children}
   </div>
 )
