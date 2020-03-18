8146460090a90e35dd57bba8550d53915cac7fb9
Luuk de Vlieger <info@luuk.site>
2020-03-18 12:06:16 +0100

    :lipstick: Responsive installation section

--- src/src/components/installation.js
+++ src/src/components/installation.js
@@ -8,15 +8,18 @@ export default () => (
       className="
         w-full
         max-w-md
-        px-8
-        py-6
-        text-lg
+        px-6
+        sm:px-8
+        py-4
+        sm:py-6
+        sm:text-lg
         rounded-lg
         bg-indigo-800
         shadow-inner
+        text-center
       "
     >
-      <span className="select-none text-blue-300 mr-4">$</span>npm install
+      <span className="select-none text-blue-300 mr-2 sm:mr-4">$</span>npm install
       hamburger-react
     </code>
   </>
