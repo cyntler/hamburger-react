5451ec2615b05e299f562eda5a93e104aa6f4e88
Luuk de Vlieger <info@luuk.site>
2020-03-19 12:31:35 +0100

    :sparkles: Add click handler

--- src/src/components/menu.js
+++ src/src/components/menu.js
@@ -8,10 +8,17 @@ export default () => (
 )
 
 const Anchor = ({ label, to }) => {
+  const clickHandler = to =>
+    typeof document !== 'undefined' &&
+    document.getElementById(to).scrollIntoView({ behavior: 'smooth' })
+
   return (
     <div
       className=""
       role="button"
+      onKeyUp={e => (e.key === 13 || e.keyCode === 13) && clickHandler(to)}
+      tabIndex="0"
+      onClick={() => clickHandler(to)}
     >
       {label}
     </div>
