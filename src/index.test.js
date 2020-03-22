76ce2e913ec9993920380cba2c530921be06970f
Luuk de Vlieger <info@luuk.site>
2020-03-22 17:46:09 +0100

    :white_check_mark: Add tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,42 @@
 import React from 'react'
-import Hamburger from '.'
+import Hamburger, { Fade, Sling, Spin, Squash, Turn, Twirl } from '.'
 import { render } from '@testing-library/react'
 
-it('renders', () => {
+it('does not throw an error', () => {
   render(<Hamburger />)
+})
+
+it('renders "Fade" version', () => {
+  const { getByTestId } = render(<Fade />)
+  getByTestId('fade')
+})
+
+it('renders "Sling" version', () => {
+  const { getByTestId } = render(<Sling />)
+  getByTestId('sling')
+})
+
+it('renders "Spin" version', () => {
+  const { getByTestId } = render(<Spin />)
+  getByTestId('spin')
+})
+
+it('renders "Squash" version', () => {
+  const { getByTestId } = render(<Squash />)
+  getByTestId('squash')
+})
+
+it('renders "Tilt" version', () => {
+  const { getByTestId } = render(<Hamburger />)
+  getByTestId('tilt')
+})
+
+it('renders "Turn" version', () => {
+  const { getByTestId } = render(<Turn />)
+  getByTestId('turn')
+})
+
+it('renders "Twirl" version', () => {
+  const { getByTestId } = render(<Twirl />)
+  getByTestId('twirl')
 })
