9a1864f168c0f73ec8d454ca3a522a7fe336efc5
Luuk de Vlieger <info@luuk.site>
2020-03-29 08:58:40 +0200

    :wrench: Update Gatsby's resolver now that React is a peer dependency

--- docs/gatsby-node.js
+++ docs/gatsby-node.js
@@ -1,12 +1,12 @@
 const path = require('path')
-const fromRoot = (name) => path.resolve(__dirname + '/../node_modules/' + name)
+const fromCurrent = (name) => path.resolve(__dirname + '/node_modules/' + name)
 
 exports.onCreateWebpackConfig = ({ actions }) =>
   actions.setWebpackConfig({
     resolve: {
       alias: {
-        react: fromRoot('react'),
-        'react-dom': fromRoot('react-dom'),
+        react: fromCurrent('react'),
+        'react-dom': fromCurrent('react-dom'),
       },
     },
   })
