37f59316d640e25313086c0d533c1d112c1fabdf
Luuk de Vlieger <info@luuk.site>
2020-04-01 15:29:56 +0200

    :speech_balloon: Naming things

--- tests/toggle.test.tsx
+++ tests/toggle.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hamburger from '../src'
 import { fireEvent, render } from '@testing-library/react'
 
-it(`fires the updater with correct arguments`, () => {
+it(`fires the action with correct arguments`, () => {
   const toggle = jest.fn()
   const { getByTestId: get } = render(<Hamburger toggled={false} toggle={toggle} />)
 
--- tests/toggled.test.tsx
+++ tests/toggled.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hamburger from '../src'
 import { render } from '@testing-library/react'
 
-it(`accepts state`, () => {
+it(`accepts state value`, () => {
   const { getByTestId: get } = render(<Hamburger toggled={true} />)
 
   expect(get('bar-two')).toHaveStyle({ transform: 'scaleX(0)' })
