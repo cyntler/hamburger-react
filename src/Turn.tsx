b43887022bd449dcbdd15d63c67ff7b508accaf7
Luuk de Vlieger <info@luuk.site>
2020-04-04 18:07:02 +0200

    :sparkles: Implement label

--- src/Turn.tsx
+++ src/Turn.tsx
@@ -6,6 +6,7 @@ export const Turn = ((props) => (
   <Burger {...props} render={(o) => (
     <div
       className="hamburger-react"
+      aria-label={o.label}
       data-testid="turn"
       onClick={o.handler}
       onKeyUp={(e) => (e.key === 'Enter' || e.keyCode === 13) && o.handler()}
