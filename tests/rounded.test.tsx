626750834448ab12a7feb99d4864c7c5c971259a
Luuk de Vlieger <info@luuk.site>
2020-05-07 14:07:41 +0200

    :white_check_mark: Add tests for 'rounded' prop

---
+++ tests/rounded.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hamburger from '../src'
+import { render, screen } from '@testing-library/react'
+
+it(`doesn't have a border radius by default`, () => {
+  render(<Hamburger />)
+
+  expect(screen.getByTestId('bar-one')).not.toHaveStyle({ borderRadius: '9em' })
+})
+
+it(`sets a border radius`, () => {
+  render(<Hamburger rounded />)
+
+  expect(screen.getByTestId('bar-one')).toHaveStyle({ borderRadius: '9em' })
+})
