f81ce5c1ad9a80dd0da6994cee0ca602fdd7fdfc
Luuk de Vlieger <info@luuk.site>
2020-05-23 13:29:03 +0200

    :speech_balloon: Docs: add size

--- docs/src/components/installation.tsx
+++ docs/src/components/installation.tsx
@@ -17,10 +17,12 @@ export default () => (
         bg-indigo-800
         shadow-inner
         text-center
+        mb-8
       "
     >
       <span className="select-none text-blue-300 mr-2 sm:mr-4">$</span>npm
       install hamburger-react
     </code>
+    <p className="sm:text-lg leading-loose text-center">When using one hamburger, ~1.5 KB will be added to your bundle (min + gzip).</p>
   </>
 )
