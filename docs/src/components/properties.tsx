218e9146a7037916b7be27bf8f9bf01f33860fa2
Luuk de Vlieger <info@luuk.site>
2020-05-23 11:29:26 +0200

    :art: Docs: change properties order

--- docs/src/components/properties.tsx
+++ docs/src/components/properties.tsx
@@ -13,124 +13,6 @@ export default () => {
 
       <div className="w-full max-w-xl">
         <Example>
-          <Property name="color" initial="currentColor" type="string" />
-          <Hamburger color="#4FD1C5" size={34} />
-          The color of the icon bars, accepts any CSS-parsable argument.
-          <div>
-            {'<'}
-            <span className="text-yellow-400">Hamburger</span>
-            &nbsp;
-            <span className="text-purple-400">color</span>="
-            <span className="text-green-400">#4FD1C5</span>
-            {'" />'}
-          </div>
-        </Example>
-
-        <Example>
-          <Property name="direction" initial="left" type="string" />
-          <Hamburger direction="right" size={26} />
-          The animation direction of the icon, left or right.
-          <div>
-            {'<'}
-            <span className="text-yellow-400">Hamburger</span>
-            &nbsp;
-            <span className="text-purple-400">direction</span>="
-            <span className="text-green-400">right</span>
-            {'" />'}
-          </div>
-        </Example>
-
-        <Example>
-          <Property name="distance" initial="md" type="string" />
-          <Hamburger distance="lg" size={32} />
-          The vertical distance between the lines. Small (sm), medium (md) or
-          large (lg).
-          <div>
-            {'<'}
-            <span className="text-yellow-400">Hamburger</span>
-            &nbsp;
-            <span className="text-purple-400">distance</span>="
-            <span className="text-green-400">lg</span>
-            {'" />'}
-          </div>
-        </Example>
-
-        <Example>
-          <Property
-            name="duration"
-            initial="0.4"
-            type="float / integer"
-            smallType="float / int"
-          />
-          <Hamburger duration={0.8} size={26} />
-          The duration of the animation. Can be set to zero if no animation is
-          desired.
-          <div>
-            {'<'}
-            <span className="text-yellow-400">Hamburger</span>
-            &nbsp;
-            <span className="text-purple-400">duration</span>={'{'}
-            <span className="text-green-400">0.8</span>
-            {'} />'}
-          </div>
-        </Example>
-
-        <Example>
-          <Property name="hideOutline" initial="true" type="boolean" />
-          <Hamburger hideOutline={false} size={30} />
-          Hides the default browser focus style.
-          <div>
-            {'<'}
-            <span className="text-yellow-400">Hamburger</span>
-            &nbsp;
-            <span className="text-purple-400">hideOutline</span>={'{'}
-            <span className="text-green-400">false</span>
-            {'} />'}
-          </div>
-        </Example>
-
-        <Example>
-          <Property name="label" initial="undefined" type="string" />
-          <Hamburger color="white" label="Show menu" size={26} />
-          An ARIA label to improve accessibility.
-          <div>
-            {'<'}
-            <span className="text-yellow-400">Hamburger</span>
-            &nbsp;
-            <span className="text-purple-400">label</span>="
-            <span className="text-green-400">Show menu</span>
-            {'" />'}
-          </div>
-        </Example>
-
-        <Example>
-          <Property name="rounded" initial="false" type="boolean" />
-          <Hamburger rounded size={42} />
-          Specifies if the icon bars should be rounded.
-          <div>
-            {'<'}
-            <span className="text-yellow-400">Hamburger</span>
-            &nbsp;
-            <span className="text-purple-400">rounded</span>
-            {' />'}
-          </div>
-        </Example>
-
-        <Example>
-          <Property name="size" initial="32" type="integer" />
-          <Hamburger size={20} />A number between 12 and 48, which sets the size
-          of the icon.
-          <div>
-            {'<'}
-            <span className="text-yellow-400">Hamburger</span>
-            &nbsp;
-            <span className="text-purple-400">size</span>={'{'}
-            <span className="text-green-400">20</span>
-            {'} />'}
-          </div>
-        </Example>
-
-        <Example>
           <div>
             <Property name="toggled" initial="undefined" type="boolean" />
             <Property name="toggle" initial="undefined" type="function" />
@@ -161,6 +43,124 @@ export default () => {
             &nbsp;
             <span className="text-purple-400">toggle</span>={'{'}
             <span className="text-green-400">setOpen</span>
+            {'} />'}
+          </div>
+        </Example>
+
+        <Example>
+          <Property name="size" initial="32" type="integer" />
+          <Hamburger size={20} />A number between 12 and 48, which sets the size
+          of the icon.
+          <div>
+            {'<'}
+            <span className="text-yellow-400">Hamburger</span>
+            &nbsp;
+            <span className="text-purple-400">size</span>={'{'}
+            <span className="text-green-400">20</span>
+            {'} />'}
+          </div>
+        </Example>
+
+        <Example>
+          <Property name="direction" initial="left" type="string" />
+          <Hamburger direction="right" size={26} />
+          The animation direction of the icon, left or right.
+          <div>
+            {'<'}
+            <span className="text-yellow-400">Hamburger</span>
+            &nbsp;
+            <span className="text-purple-400">direction</span>="
+            <span className="text-green-400">right</span>
+            {'" />'}
+          </div>
+        </Example>
+
+        <Example>
+          <Property
+            name="duration"
+            initial="0.4"
+            type="float / integer"
+            smallType="float / int"
+          />
+          <Hamburger duration={0.8} size={26} />
+          The duration of the animation. Can be set to zero if no animation is
+          desired.
+          <div>
+            {'<'}
+            <span className="text-yellow-400">Hamburger</span>
+            &nbsp;
+            <span className="text-purple-400">duration</span>={'{'}
+            <span className="text-green-400">0.8</span>
+            {'} />'}
+          </div>
+        </Example>
+
+        <Example>
+          <Property name="distance" initial="md" type="string" />
+          <Hamburger distance="lg" size={32} />
+          The vertical distance between the lines. Small (sm), medium (md) or
+          large (lg).
+          <div>
+            {'<'}
+            <span className="text-yellow-400">Hamburger</span>
+            &nbsp;
+            <span className="text-purple-400">distance</span>="
+            <span className="text-green-400">lg</span>
+            {'" />'}
+          </div>
+        </Example>
+
+        <Example>
+          <Property name="color" initial="currentColor" type="string" />
+          <Hamburger color="#4FD1C5" size={34} />
+          The color of the icon bars, accepts any CSS-parsable argument.
+          <div>
+            {'<'}
+            <span className="text-yellow-400">Hamburger</span>
+            &nbsp;
+            <span className="text-purple-400">color</span>="
+            <span className="text-green-400">#4FD1C5</span>
+            {'" />'}
+          </div>
+        </Example>
+
+        <Example>
+          <Property name="rounded" initial="false" type="boolean" />
+          <Hamburger rounded size={42} />
+          Specifies if the icon bars should be rounded.
+          <div>
+            {'<'}
+            <span className="text-yellow-400">Hamburger</span>
+            &nbsp;
+            <span className="text-purple-400">rounded</span>
+            {' />'}
+          </div>
+        </Example>
+
+        <Example>
+          <Property name="label" initial="undefined" type="string" />
+          <Hamburger color="white" label="Show menu" size={26} />
+          An ARIA label to improve accessibility.
+          <div>
+            {'<'}
+            <span className="text-yellow-400">Hamburger</span>
+            &nbsp;
+            <span className="text-purple-400">label</span>="
+            <span className="text-green-400">Show menu</span>
+            {'" />'}
+          </div>
+        </Example>
+
+        <Example>
+          <Property name="hideOutline" initial="true" type="boolean" />
+          <Hamburger hideOutline={false} size={30} />
+          Hides the default browser focus style.
+          <div>
+            {'<'}
+            <span className="text-yellow-400">Hamburger</span>
+            &nbsp;
+            <span className="text-purple-400">hideOutline</span>={'{'}
+            <span className="text-green-400">false</span>
             {'} />'}
           </div>
         </Example>
