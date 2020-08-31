274884b9b37f0ea7b38fc61bdf16da028677cd6d
Luuk de Vlieger <info@luuk.site>
2020-08-31 20:44:39 +0200

    :white_check_mark: Add tests for 'easing' prop

---
+++ tests/easing.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hamburger from '../src'
+import { render, screen } from '@testing-library/react'
+
+it(`has a default cubic timing`, () => {
+  render(<Hamburger duration={0.2} />)
+
+  expect(screen.getByTestId('bar-one')).toHaveStyle({ transition: '0.2s cubic-bezier(0, 0, 0, 1)' })
+})
+
+it(`sets the correct size`, () => {
+  render(<Hamburger duration={0.2} easing="ease-out" />)
+
+  expect(screen.getByTestId('bar-one')).toHaveStyle({ transition: '0.2s ease-out' })
+})
