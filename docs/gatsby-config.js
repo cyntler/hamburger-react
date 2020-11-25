ae34bd39d167e3bdf97c77c4019d11a80c2c5ba5
Luuk de Vlieger <info@luuk.site>
2020-11-25 09:41:17 +0100

    :heavy_minus_sign: Remove Gatsby purge plugin

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -2,13 +2,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-netlify',
     'gatsby-plugin-postcss',
-    {
-      resolve: 'gatsby-plugin-purgecss',
-      options: {
-        purgeOnly: ['styles/index.css'],
-        tailwind: true,
-      },
-    },
     'gatsby-plugin-typescript',
   ],
 }
