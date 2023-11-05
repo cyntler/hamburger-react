8fb661a9afb416d2217be098b8e5b0c35ef6c142
Damian Cyntler <damian@cyntler.com>
2023-11-05 23:19:53 +0100

    docs: update deps & prettier fix

--- docs/astro.config.js
+++ docs/astro.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind(), // https://github.com/withastro/astro/blob/main/packages/integrations/tailwind/src/index.ts
-  ],
+  integrations: [react(), tailwind()],
 });
--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -1,20 +1,20 @@
-const colors = require("./colors.cjs");
+const colors = require('./colors.cjs');
 
 module.exports = {
-  content: ["./**/*.{astro,jsx}"],
+  content: ['./**/*.{astro,jsx}'],
   theme: {
     colors,
     fontSize: {
-      xs: "0.75rem",
-      sm: "0.875rem",
-      base: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "3xl": "1.875rem",
-      "4xl": "2.25rem",
-      "5xl": "3rem",
-      "6xl": "4rem",
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
     },
   },
 };
